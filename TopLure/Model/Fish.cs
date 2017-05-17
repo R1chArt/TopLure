f25a98a41a8b8be4a38a7ab9d96255e319fce236
RiP <RiP@none.exist>
2017-05-17 08:35:03 +0200

    basic's and packages

---
+++ TopLure/Model/Fish.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopLure.Model
+{
+    public class Fish
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private float weight;
+
+        public float Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        private float length;
+
+        public float Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+    }
+}
---
+++ TopLure/Model/Lure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopLure.Model
+{
+    public class Lure
+    {
+        private string pattern;
+
+        public string Pattern
+        {
+            get { return pattern; }
+            set { pattern = value; }
+        }
+
+        private string style;
+
+        public string Style
+        {
+            get { return style; }
+            set { style = value; }
+        }
+
+        private string size;
+
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+    }
+}
